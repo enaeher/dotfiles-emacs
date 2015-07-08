59392e35bbedf8303f50ba3e29875f52b847979f
Eli Naeher <eli@naeher.name>
2015-07-07 19:53:55 -0500

    Various changes

--- .emacs.d/lisp/clojure.el
+++ .emacs.d/lisp/clojure.el
@@ -1,23 +1,34 @@
 (use-package clojure-mode
+    :ensure t
+    :init (add-hook 'clojure-mode-hook 'turn-on-eldoc-mode))
+
+(use-package aggressive-indent
+  :ensure t
+  :init (add-hook 'clojure-mode-hook (lambda ()
+				       (aggressive-indent-mode 1))))
+
+(use-package paredit
+  :ensure t
+  :init (progn
+	  (add-hook 'clojure-mode-hook (lambda () (paredit-mode 1)))
+	  (add-hook 'cider-repl-mode-hook (lambda () (paredit-mode 1)))))
+
+(use-package clj-refactor
+  :ensure t
   :init (add-hook 'clojure-mode-hook (lambda ()
 				       (clj-refactor-mode 1)
-				       (aggressive-indent-mode 1)
-				       (paredit-mode 1))))
+				       (cljr-add-keybindings-with-prefix "C-c C-m")
+				       (dolist (mapping '(("json" . "cheshire.core")
+							  ("async" . "clojure.core.async")))
+					 (add-to-list 'cljr-magic-require-namespaces mapping t)))))
 
 (use-package cider
-  :ensure t
-  :init
-  (progn
-    (cljr-add-keybindings-with-prefix "C-c C-m")
-    (add-hook 'nrepl-connected-hook #'cljr-update-artifact-cache)
-    (add-hook 'nrepl-connected-hook #'cljr-warm-ast-cache)
-    (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-    (add-hook 'cider-mode-hook 'company-mode)
-    (add-hook 'cider-repl-mode-hook 'company-mode)
-    (setq nrepl-hide-special-buffers nil)))
+  :ensure t)
 
-(use-package squiggly-clojure
+(use-package flycheck-clojure
+  :ensure t
   :init (add-hook 'cider-mode-hook (lambda () (flycheck-mode 1))))
 
 (use-package flycheck
+  :ensure t
   :init (flycheck-clojure-setup))
--- .emacs.d/lisp/macosx-clipboard.el
+++ .emacs.d/lisp/macosx-clipboard.el
@@ -1,4 +1,5 @@
 ;(setq ns-function-modifier 'super)
 
 (use-package pbcopy
+  :ensure t
   :init (turn-on-pbcopy))
