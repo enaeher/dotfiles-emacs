5f638a63964825d3893cf77f2c743311560847f5
Eli Naeher <eli@naeher.name>
2016-07-13 09:50:52 -0400

    Stuff

--- .emacs.d/lisp/clojure.el
+++ .emacs.d/lisp/clojure.el
@@ -5,33 +5,30 @@
 (use-package aggressive-indent
   :ensure t
   :init (add-hook 'clojure-mode-hook (lambda ()
-				       (aggressive-indent-mode 1))))
+                                       (aggressive-indent-mode 1))))
 
 (use-package paredit
   :ensure t
   :init (progn
-	  (add-hook 'clojure-mode-hook (lambda () (paredit-mode 1)))
-	  (add-hook 'cider-repl-mode-hook (lambda () (paredit-mode 1)))))
+          (add-hook 'clojure-mode-hook (lambda () (paredit-mode 1)))
+          (add-hook 'cider-repl-mode-hook (lambda () (paredit-mode 1)))))
 
 (use-package clj-refactor
   :ensure t
   :init (add-hook 'clojure-mode-hook (lambda ()
-				       (clj-refactor-mode 1)
-				       (cljr-add-keybindings-with-prefix "C-c C-m")
-				       (dolist (mapping '(("json" . "cheshire.core")
-							  ("async" . "clojure.core.async")))
-					 (add-to-list 'cljr-magic-require-namespaces mapping t)))))
+                                       (clj-refactor-mode 1)
+                                       (cljr-add-keybindings-with-prefix "C-c C-m")
+                                       (progn
+                                         (put-clojure-indent 'fact 'defun)
+                                         (put-clojure-indent 'facts 'defun)
+                                         (put-clojure-indent 'future-fact 'defun)
+                                         (put-clojure-indent 'future-facts 'defun))
+                                       (dolist (mapping '(("json" . "cheshire.core")
+                                                          ("async" . "clojure.core.async")))
+                                         (add-to-list 'cljr-magic-require-namespaces mapping t)))))
 
 (use-package cider
   :ensure t)
-
-(use-package flycheck-clojure
-  :ensure t
-  :init (add-hook 'cider-mode-hook (lambda () (flycheck-mode 1))))
-
-(use-package flycheck
-  :ensure t
-  :init (flycheck-clojure-setup))
 
 (use-package rainbow-delimiters
     :ensure t
